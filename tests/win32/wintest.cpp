6894b056703013eacd500e1abb7bd2bb95056289
Alexander Feodorov <webmaster@unixcomp.org>
2022-01-19 23:00:22 +0300

    Fix

--- tests/win32/wintest.cpp
+++ tests/win32/wintest.cpp
@@ -1,3 +1,7 @@
+// This code is not protected by any copyright as far as I know
+// It is based on misc public sources on the Internet
+// Prepared by Kitel Boris in 2021
+
 #include <stdio.h>
 
 int main() {
